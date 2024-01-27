52a6b59a77755f73f89858f37223ba433d5c8689
Jose Valero <jvalero000@gmail.com>
2024-01-27 10:23:34 -0700

    Add zsh install

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
+# Install prerequisites
+sudo apt install git curl
+
+# Install zsh, oh-my-zsh, and powerlevel10k
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # Bootstrap tmux
 sudo apt install tmux
 git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
